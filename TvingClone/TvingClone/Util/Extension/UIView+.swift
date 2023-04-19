626119c74d841e60518f67a8637db07d120f582b
ahra1221 <lizze011221@gmail.com>
2023-04-19 22:24:49 +0900

    [Feat] addView extension 구현

--- TvingClone/TvingClone/Util/Extension/UIView+.swift
+++ TvingClone/TvingClone/Util/Extension/UIView+.swift
@@ -5,4 +5,13 @@
 //  Created by 고아라 on 2023/04/19.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addSubview($0)
+        }
+    }
+}
