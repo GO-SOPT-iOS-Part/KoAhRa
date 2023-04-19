e886b284f16e2555a124938fb148274d8d030095
ahra1221 <lizze011221@gmail.com>
2023-04-19 22:28:11 +0900

    [Feat] textfield extension 구현

--- TvingClone/TvingClone/Util/Extension/UITextField+.swift
+++ TvingClone/TvingClone/Util/Extension/UITextField+.swift
@@ -5,4 +5,29 @@
 //  Created by 고아라 on 2023/04/19.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    
+    func setPlaceholderColor(_ placeholderColor: UIColor) {
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [
+                .foregroundColor: placeholderColor,
+                .font: font
+            ].compactMapValues { $0 }
+        )
+    }
+    
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
