34233161f43cf6b1397a6401a1e6ca3fb95f89a9
ahra1221 <lizze011221@gmail.com>
2023-04-19 22:28:53 +0900

    [Feat] button extension 구현

--- TvingClone/TvingClone/Util/Extension/UIButton+.swift
+++ TvingClone/TvingClone/Util/Extension/UIButton+.swift
@@ -5,4 +5,17 @@
 //  Created by 고아라 on 2023/04/19.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
