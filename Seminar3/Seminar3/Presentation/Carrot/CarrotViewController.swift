a8b1dd408987e0ecda13a5dd29c16ba5cf4a78c8
ahra1221 <lizze011221@gmail.com>
2023-04-29 18:31:08 +0900

    [Feat] CarrotViewController 구현

--- Seminar3/Seminar3/Presentation/Carrot/CarrotViewController.swift
+++ Seminar3/Seminar3/Presentation/Carrot/CarrotViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class CarrotViewController: UIViewController {
+import SnapKit
+import Then
+
+final class CarrotViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummy.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CarrotTableViewCell.identifier, for: indexPath) as? CarrotTableViewCell else { return UITableViewCell() }
+        
+        cell.configureCell(dummy[indexPath.row])
+        
+        return cell
+    }
+    
+    
+    private let carrotTableView = UITableView()
+    private let dummy = Carrot.dummy()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setLayout()
+        setStyle()
+    }
+
+}
+
+extension CarrotViewController {
+    
+    private func setLayout() {
+        view.backgroundColor = .white
+        
+        view.addSubview(carrotTableView)
+        carrotTableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setStyle() {
+        carrotTableView.do {
+            $0.register(CarrotTableViewCell.self, forCellReuseIdentifier: CarrotTableViewCell.identifier)
+            $0.rowHeight = 120
+            $0.delegate = self
+            $0.dataSource = self
+        }
     }
-    */
-
 }
