07d6a5d83400a45e9c54390334242f59da685f16
ahra1221 <lizze011221@gmail.com>
2023-04-29 17:05:32 +0900

    [Setting] 초기 세팅

---
+++ Seminar3/Seminar3/Presentation/Carrot/CarrotViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CarrotViewController.swift
+//  Seminar3
+//
+//  Created by 고아라 on 2023/04/29.
+//
+
+import UIKit
+
+class CarrotViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Seminar3/Seminar3/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar3
+//
+//  Created by 고아라 on 2023/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
