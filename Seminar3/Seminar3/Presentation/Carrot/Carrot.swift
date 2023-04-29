1edcb0b9a8b38fe5626e6e2747f36c42df15089a
ahra1221 <lizze011221@gmail.com>
2023-04-29 17:30:24 +0900

    [Add] 더미 데이터 추가

--- Seminar3/Seminar3/Presentation/Carrot/Carrot.swift
+++ Seminar3/Seminar3/Presentation/Carrot/Carrot.swift
@@ -9,7 +9,7 @@ import UIKit
 
 struct Carrot {
     let image : UIImage
-    let title : String
+    let product : String
     let place : String
     let time : String
     let tradeStatus : Trade
@@ -47,5 +47,61 @@ enum Trade {
         case .clear :
             return .white
         }
+    }
+}
+
+extension Carrot {
+    
+    static func dummy() -> [Carrot] {
+        return [Carrot(image: .productImage,
+                       product: "에어팟 프로 2",
+                       place: "하계 2동",
+                       time: "일주일 전",
+                       tradeStatus: .completed,
+                       price: 200000),
+                Carrot(image: .productImage,
+                       product:
+                        "아이폰 11 Pro",
+                       place: "하계 3동",
+                       time: "4월 2일",
+                       tradeStatus: .reservation,
+                       price: 100000),
+                Carrot(image: .productImage,
+                       product:
+                        "에어팟 맥스",
+                       place: "공릉 2동",
+                       time: "1일 전",
+                       tradeStatus: .shared,
+                       price: 100000),
+                Carrot(image: .productImage,
+                       product:
+                        "아이패드1",
+                       place: "공릉 1동",
+                       time: "6시간 전",
+                       tradeStatus: .completed,
+                       price: 100000),
+                Carrot(image: .productImage,
+                       product:
+                        "아이패드 2",
+                       place: "중계 2동",
+                       time: "3시간 전",
+                       tradeStatus: .clear,
+                       price: 100000)
+                ,
+                Carrot(image: .productImage,
+                       product:
+                        "안팔아요",
+                       place: "중계 2동",
+                       time: "4월 3일",
+                       tradeStatus: .completed,
+                       price: 10000)
+                ,
+                Carrot(image: .productImage,
+                       product:
+                        "아이폰 14 Pro",
+                       place: "중계 2동",
+                       time: "3월 31일",
+                       tradeStatus: .reservation,
+                       price: 1000)]
     }
 }
---
+++ Seminar3/Seminar3/Util/Extension/UIImage+.swift
@@ -0,0 +1,12 @@
+//
+//  Image.swift
+//  Seminar3
+//
+//  Created by 고아라 on 2023/04/29.
+//
+
+import UIKit
+
+public enum Image {
+    public static let hypeboy = UIImage(named: "hypeboy")!
+}
