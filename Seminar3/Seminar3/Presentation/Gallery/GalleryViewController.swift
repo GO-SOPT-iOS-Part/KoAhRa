8997eea46a485ee3c1cf0c012878726932f98836
ahra1221 <lizze011221@gmail.com>
2023-05-02 11:12:35 +0900

    [Feat] gallery 뷰 컨트롤러 기능 구현

--- Seminar3/Seminar3/Presentation/Gallery/GalleryViewController.swift
+++ Seminar3/Seminar3/Presentation/Gallery/GalleryViewController.swift
@@ -7,23 +7,58 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return dummyCase.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GalleryCollectionViewCell.identifier, for: indexPath) as? GalleryCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.configureCell(dummyCase[indexPath.item])
+        
+        return cell
+    }
+    
+    
+    private lazy var collectionView = UICollectionView(frame: .zero,
+                                                      collectionViewLayout: flowLayout)
+    private let flowLayout = UICollectionViewFlowLayout()
+    private var dummyCase = Photo.dummy()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setStyle()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func setStyle() {
+        
+        collectionView.do {
+            $0.register(GalleryCollectionViewCell.self, forCellWithReuseIdentifier: GalleryCollectionViewCell.identifier)
+            $0.showsVerticalScrollIndicator = false
+            $0.delegate = self
+            $0.dataSource = self
+        }
+        
+        flowLayout.do {
+            $0.itemSize = CGSize(width: (UIScreen.main.bounds.width - 6) / 3 , height: (UIScreen.main.bounds.width - 6) / 3)
+            $0.minimumLineSpacing = 3
+            $0.minimumInteritemSpacing = 3
+        }
     }
-    */
-
+    
+    func setLayout() {
+        
+        view.addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
