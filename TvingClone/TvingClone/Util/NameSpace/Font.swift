d33e61f1a62532c011ec1b27fc489afa79b2d6e6
ahra1221 <lizze011221@gmail.com>
2023-05-03 22:49:07 +0900

    [Add] 폰트 리소스 추가

--- TvingClone/TvingClone/Util/NameSpace/Font.swift
+++ TvingClone/TvingClone/Util/NameSpace/Font.swift
@@ -12,6 +12,8 @@ public enum Font {
     public static let tvingBold1 = UIFont.pretendard(.bold, size: 23)
     public static let tvingBold2 = UIFont.pretendard(.bold, size: 15)
     public static let tvingMedium = UIFont.pretendard(.medium, size: 23)
+    public static let tvingMedium2 = UIFont.pretendard(.medium, size: 17)
+    public static let tvingMedium3 = UIFont.pretendard(.medium, size: 10)
     public static let tvingRegular = UIFont.pretendard(.regular, size: 14)
     public static let tvingSemiBold1 = UIFont.pretendard(.semibold, size: 15)
     public static let tvingSemiBold2 = UIFont.pretendard(.semibold, size: 14)
