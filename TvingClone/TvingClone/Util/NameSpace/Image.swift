dbc85e26a6a4b658201e1974c9766d765679b40d
ahra1221 <lizze011221@gmail.com>
2023-05-03 22:04:17 +0900

    [Add] 이미지 리소스 추가

--- TvingClone/TvingClone/Util/NameSpace/Image.swift
+++ TvingClone/TvingClone/Util/NameSpace/Image.swift
@@ -10,8 +10,12 @@ import UIKit
  public enum Image {
      
      public static let backButtonIcon = UIImage(named: "btn_before")!
+     public static let alarmButtonIcon = UIImage(named: "btn_alarm")!
+     public static let settingButtonIcon = UIImage(named: "btn_setting")!
      public static let passwordShowIcon = UIImage(named: "eye")!
      public static let passwordHideIcon = UIImage(named: "eye-slash")!
      public static let deleteIcon = UIImage(named: "x-circle")!
      public static let tivingLogo = UIImage(named: "logo")!
+     public static let tivingMainLogo = UIImage(named: "main_logo")!
+     public static let profileLogo = UIImage(named: "profile")!
  }
