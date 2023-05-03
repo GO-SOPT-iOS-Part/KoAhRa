5d6118148bf9a3fed09a6ec02daace35bcf95b59
ahra1221 <lizze011221@gmail.com>
2023-05-03 22:56:09 +0900

    [Add] 이미지 리소스 추가

--- TvingClone/TvingClone/Util/NameSpace/Image.swift
+++ TvingClone/TvingClone/Util/NameSpace/Image.swift
@@ -15,7 +15,11 @@ import UIKit
      public static let passwordShowIcon = UIImage(named: "eye")!
      public static let passwordHideIcon = UIImage(named: "eye-slash")!
      public static let deleteIcon = UIImage(named: "x-circle")!
+     public static let ticketIcon = UIImage(named: "ticket")!
+     public static let cashIcon = UIImage(named: "cash")!
      public static let tivingLogo = UIImage(named: "logo")!
      public static let tivingMainLogo = UIImage(named: "main_logo")!
+     public static let tvnLogo = UIImage(named: "tvn_logo")!
+     public static let jtbcLogo = UIImage(named: "jtbc_logo")!
      public static let profileLogo = UIImage(named: "profile")!
  }
