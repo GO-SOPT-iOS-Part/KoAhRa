b3c055f501a4c64f41710437750477865c7da79e
ahra1221 <lizze011221@gmail.com>
2023-05-12 11:22:52 +0900

    [Feat] 이미지 가져오는 코드 구현

---
+++ Seminar4/Seminar4/Network/Foundation/ImageCacheManager.swift
@@ -0,0 +1,62 @@
+//
+//  ImageCacheManager.swift
+//  Seminar4
+//
+//  Created by 고아라 on 2023/05/10.
+//
+
+import UIKit
+
+enum ImageNetworkError: Error {
+    case `default`
+}
+
+class ImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    private init() { }
+    
+    static func getImage (
+        _ url: String,
+        completion: @escaping (Result<UIImage, ImageNetworkError>) -> Void
+    ) {
+        if url.isEmpty { completion(.failure(.default)) }
+        let cachedKey = NSString(string: url)
+        
+        if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey) {
+            completion(.success(cachedImage))
+        }
+        
+        guard let url = URL(string: url) else {
+            completion(.failure(.default))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(.failure(.default))
+                }
+                if let data = data, let image = UIImage(data: data) {
+                    ImageCacheManager.shared.setObject(image, forKey: cachedKey)
+                    completion(.success(image))
+                }
+            }
+        }
+        .resume()
+    }
+}
+
+extension UIImageView {
+    func getImageFromURL(_ url: String) {
+        let imageURL = "http://openweathermap.org/img/wn/" + url + "@2x.png"
+        ImageCacheManager.getImage(imageURL) { result in
+            switch result {
+            case .failure(.default):
+                print("fail")
+                self.image = UIImage()
+            case .success(let image):
+                self.image = image
+            }
+        }
+    }
+}
---
+++ Seminar4/Seminar4/Presentation/OpenWeather/OpenWeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  OpenWeatherTableViewCell.swift
+//  Seminar4
+//
+//  Created by 고아라 on 2023/05/10.
+//
+
+import UIKit
+
+class OpenWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
