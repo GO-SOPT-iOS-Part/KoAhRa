0f95a48cc93e6f1e66160218e72f021248f6c40c
ahra1221 <lizze011221@gmail.com>
2023-05-12 12:37:47 +0900

    [Feat] movie datamodel 생성

---
+++ Seminar4/Seminar4/Network/APIService/MovieService.swift
@@ -0,0 +1,8 @@
+//
+//  MovieService.swift
+//  Seminar4
+//
+//  Created by 고아라 on 2023/05/12.
+//
+
+import Foundation
---
+++ Seminar4/Seminar4/Presentation/Movie/MovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieTableViewCell.swift
+//  Seminar4
+//
+//  Created by 고아라 on 2023/05/12.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
