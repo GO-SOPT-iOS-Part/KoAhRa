9cfa94a0b7f2b6d115bc6300dbe587149ac70d4b
ahra1221 <lizze011221@gmail.com>
2023-05-12 12:40:48 +0900

    [Feat] movie viewcontroller 구현 완료

--- Seminar4/Seminar4/Network/Foundation/MovieImageCacheManager.swift
+++ Seminar4/Seminar4/Network/Foundation/MovieImageCacheManager.swift
@@ -5,4 +5,59 @@
 //  Created by 고아라 on 2023/05/12.
 //
 
-import Foundation
+import UIKit
+
+enum ImageNetworkError: Error {
+    case `default`
+}
+
+class MovieImageCacheManager {
+    static let shared = NSCache<NSString, UIImage>()
+    private init() { }
+    
+    static func getImage (
+        _ url: String,
+        completion: @escaping (Result<UIImage, ImageNetworkError>) -> Void
+    ) {
+        if url.isEmpty { completion(.failure(.default)) }
+        let cachedKey = NSString(string: url)
+        
+        if let cachedImage = ImageCacheManager.shared.object(forKey: cachedKey) {
+            completion(.success(cachedImage))
+        }
+        
+        guard let url = URL(string: url) else {
+            completion(.failure(.default))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(.failure(.default))
+                }
+                if let data = data, let image = UIImage(data: data) {
+                    ImageCacheManager.shared.setObject(image, forKey: cachedKey)
+                    completion(.success(image))
+                }
+            }
+        }
+        .resume()
+    }
+}
+
+extension UIImageView {
+    func getImageFromURL(_ url: String) {
+        let imageURL = "http://openweathermap.org/img/wn/" + url + "@2x.png"
+        ImageCacheManager.getImage(imageURL) { result in
+            switch result {
+            case .failure(.default):
+                print("fail")
+                self.image = UIImage()
+            case .success(let image):
+                self.image = image
+            }
+        }
+    }
+}
+
--- Seminar4/Seminar4/Presentation/Movie/MovieViewController.swift
+++ Seminar4/Seminar4/Presentation/Movie/MovieViewController.swift
@@ -7,23 +7,74 @@
 
 import UIKit
 
-class MovieViewController: UIViewController {
+import SnapKit
+import Then
+
+final class MovieViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    private var movieData: [MovieResponse] = []
+    private let idArray: [Int] = [540,541,542,543,544,546,547,548,549,550,551,552,553,554,555]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        movie()
+        setRegister()
+        setLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func movie() {
+        
+        idArray.forEach {
+            MovieService.shared.movie(id: $0) { response in
+                switch response {
+                case .success(let data) :
+                    self.movieData.append(data as! MovieResponse)
+                    self.tableView.reloadData()
+                default :
+                    break
+                }
+            }
+        }
     }
-    */
+    
+    private func setRegister() {
+        tableView.do {
+            $0.register(MovieTableViewCell.self, forCellReuseIdentifier: MovieTableViewCell.identifier)
+            $0.rowHeight = 120
+            $0.delegate = self
+            $0.dataSource = self
+        }
+    }
+    
+    private func setLayout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaInsets)
+            $0.bottom.leading.trailing.equalToSuperview()
+        }
+    }
 
 }
+
+extension MovieViewController: UITableViewDelegate {}
+
+extension MovieViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return idArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier, for: indexPath) as? MovieTableViewCell else { return UITableViewCell() }
+        
+        if (movieData.count == idArray.count) {
+            cell.configureCell(movieData[indexPath.row])
+        }
+        
+        return cell
+    }
+}
+
